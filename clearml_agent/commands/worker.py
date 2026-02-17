920f3c123ccfeb5dcde52b79120a4c6d87b24553
Matteo Destro <matteo@cerrion.com>
2026-02-17 10:30:19 +0000

    Handle excpetion in is_file

--- clearml_agent/commands/worker.py
+++ clearml_agent/commands/worker.py
@@ -3511,7 +3511,12 @@ class Worker(ServiceCommandSection):
                     task=task, vcs=vcs, execution_info=execution, repo_info=repo_info
                 )
             script_file = Path(directory) / execution.entry_point
-            if not script_file.is_file():
+
+            try:
+                is_file = script_file.is_file()
+            except OSError:
+                is_file = False
+            if is_file:
                 # try to parse with shlex
                 try:
                     script_file = Path(directory) / shlex.split(execution.entry_point)[0]
